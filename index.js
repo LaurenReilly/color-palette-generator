ce0101e28e17ea67cd601b79379452209f8724f8
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-18 14:11:16 -0500

    sliders working, added example page to the site which uses the generated colors, added custom fonts, added header

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ class Colors extends Component {
         this.state = {            
             colors: [
                 {
-                    color: "rgb(255,255,255)",
+                    color: "rgb(200,200,200)",
                     isLocked: false
                 },
                 {
-                    color: "rgb(255,255,255)",
+                    color: "rgb(200,200,200)",
                     isLocked: false
                 },
                 {
-                    color: "rgb(255,255,255)",
+                    color: "rgb(200,200,200)",
                     isLocked: false
                 },
                 {
-                    color: "rgb(255,255,255)",
+                    color: "rgb(200,200,200)",
                     isLocked: false
                 },
                 {
-                    color: "rgb(255,255,255)",
+                    color: "rgb(200,200,200)",
                     isLocked: false
                 }
             ]
@@ -37,13 +37,16 @@ class Colors extends Component {
         let r = this.randomColor();
             this.setState({
             colors: this.state.colors.map((color, i) => {
+                if (color.isLocked === true) {
+                    return color
+                } else {
                 let g = this.randomColor();
                 let b = this.randomColor();
                 let newColor = "rgb("+r+","+g+","+b+")";
-                return {...color, color:newColor}
+                return {...color, color: newColor}
+                }
             })
         })
-
     }
     //toggle isLocked on each individual color
     lockColor(index) {
@@ -74,6 +77,7 @@ class Colors extends Component {
             })
         })
     }
+    //change individual color channels using sliders for r, g, and b
     slider(index) {
         console.log("inside slider fn");
         let colorValue = event.target.value;
@@ -110,7 +114,7 @@ class Colors extends Component {
     }
     render() {
         return (
-            <div className="mt-5 d-flex flex-column">
+            <div className="mt-3 d-flex flex-column">
                 <div className="d-flex">
                    {this.state.colors.map((color, index) => {
                        return <ColorBlock 
@@ -126,18 +130,18 @@ class Colors extends Component {
                 <button style={{width:"10em"}} 
                         className="btn-lg btn-success m-auto" 
                         onClick={() => {this.generateColors()}}>Generate Colors</button>
-                <div className="text-center mt-1">
+                <div className="text-center mt-3">
                     <div className="m-auto d-flex flex-column justify-content-center rounded" 
-                         style={{backgroundColor:this.state.colors[0].color, width:"80%", height:"7em"}}>
+                         style={{backgroundColor:this.state.colors[0].color, width:"60%", height:"6em", boxShadow: "5px 5px 2px rgb(64,64,64)"}}>
                         <h2 className="text-center" 
-                            style={{color: this.state.colors[1].color, fontSize: "5em", textShadow:"2px 2px 2px white"}}>Example</h2>
+                            style={{color: this.state.colors[1].color, textShadow:"2px 2px 2px white", fontSize:"4em"}}>Example</h2>
                     </div>
                     <div className="text-center m-auto rounded" 
-                        style={{backgroundColor: this.state.colors[2].color, width:"80%", height:"12em"}}>
+                        style={{backgroundColor: this.state.colors[2].color, width:"60%", height:"14em", boxShadow: "5px 5px 2px rgb(64,64,64)"}}>
                         <p className="mt-3 p-4" 
-                            style={{color: this.state.colors[3].color, fontSize:"1.3em"}}>You've got it figured out. Aesthetic color schemes are a thing of the past.<br/> Why show them something "beautiful" when you can show them something...</p>
+                            style={{color: this.state.colors[3].color, fontSize:"1.2em"}}>You've got it figured out. Aesthetic color schemes are a thing of the past.<br/> Why show them something "beautiful" when you can show them something...</p>
                         <h3 className="text-center" 
-                            style={{color: this.state.colors[4].color, fontSize:"3em", textShadow:"2px 2px 2px white"}}>REVOLUTIONARY</h3>
+                            style={{color: this.state.colors[4].color, fontSize:"4em", textShadow:"2px 2px 2px white", fontFamily: "'Monoton', cursive"}}>REVOLUTIONARY</h3>
                     </div>
                 </div>
             </div>
@@ -147,21 +151,21 @@ class Colors extends Component {
 
 let ColorBlock = (props) => {
         return (
-            <div style={{backgroundColor: props.color, width:"20%", height:"20em"}} className="rounded m-1 d-flex flex-column align-content-between">
+            <div style={{backgroundColor: props.color, width:"20%", height:"20em", boxShadow: "5px 5px 2px rgb(64,64,64)"}} className="rounded mx-1 mb-3 d-flex flex-column align-content-between">
                 <h3 className="text-center mt-5" style={{color:"white"}}>{props.color}</h3>
-                <button className="btn rounded btn-large mb-1 btn-success" onClick={() => props.lockColor()}>{props.isLocked+""}</button>
+                {props.isLocked ? <i className="mb-1 fa fa-lock text-center fa-2x" onClick={() => props.lockColor()}></i> : <i className="mb-1 fa fa-unlock-alt text-center fa-2x" onClick={() => props.lockColor()}></i>}
                 <button className="btn rounded btn-large mt-1 mb-5 btn-primary" onClick={() => props.randomize()}>Randomize</button>
-                <div>
-                    <label htmlFor="redRange">Red </label>
-                    <input type="range" min="0" max="255" id="redRange" onChange={(event) => props.slider()}/>
+                <div className="m-auto" style={{width: "80%"}}>
+                    <label htmlFor="redRange" style={{float:"left", color:"white"}}>Red </label>
+                    <input style={{float:"right"}} type="range" min="0" max="255" id="redRange" onChange={(event) => props.slider()}/>
                 </div>
-                <div>
-                    <label htmlFor="greenRange">Green </label>
-                    <input type="range" min="0" max="255" id="greenRange" onChange={(event) => props.slider()}/>
+                <div className="m-auto" style={{width: "80%"}}>
+                    <label htmlFor="greenRange" style={{float:"left", color:"white"}}>Green </label>
+                    <input style={{float:"right"}} type="range" min="0" max="255" id="greenRange" onChange={(event) => props.slider()}/>
                 </div>
-                <div>
-                    <label htmlFor="blueRange">Blue </label>
-                    <input type="range" min="0" max="255" id="blueRange" onChange={(event) => props.slider()}/>
+                <div className="m-auto" style={{width: "80%"}}>
+                    <label htmlFor="blueRange" style={{float:"left", color:"white"}}>Blue </label>
+                    <input style={{float:"right"}} type="range" min="0" max="255" id="blueRange" onChange={(event) => props.slider()}/>
                 </div>
             </div>
         )
